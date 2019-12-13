152e56c686b58aeb39ad9d4d0adf952b8b33c9b1
Ufuoma Okoro <ufookoro@gmail.com>
2019-12-13 20:36:10 +0000

    Initial Commit

---
+++ Urhobo_Game_V2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Urhobo_Game_V2
+//
+//  Created by Ufuoma Okoro on 13/12/2019.
+//  Copyright © 2019 Ufuoma Okoro. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
